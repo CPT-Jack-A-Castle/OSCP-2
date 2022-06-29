46183537c2b1d3dd9aa725e3841dd112a17725e1
Stuar-97 <stuartfranklin17@gmail.com>
2022-06-29 15:12:19 -0400

    added windows arc checker

---
+++ miscellaneous/windows-arc-checker.ps1
@@ -0,0 +1,33 @@
+﻿$m1=$env:PROCESSOR_ARCHITECTURE
+$m2=$(Get-WmiObject Win32_OperatingSystem).OSArchitecture
+$m3=$(wmic os get OSArchitecture)[2]
+$m4=if([System.IntPtr]::Size -eq 4){
+    "32-bit"
+    }else{
+        "64-bit"
+        }
+
+echo ==========================================
+echo " "
+
+echo "DETERMINES IF THE OS IS 32bit or 64bit"
+echo " "
+echo ==========================================
+
+echo " "
+
+
+ 
+echo "running the command $env:PROCESSOR_ARCHITECTURE...architecture: $m1"
+
+echo " "
+
+echo "running the command (Get-WmiObject Win32_OperatingSystem).OSArchitecture...architecture: $m2"
+
+echo " "
+
+echo "running the command (wmic os get OSArchitecture)[2]...architecture: $m3"
+
+echo " "
+
+echo "running the if statement...architecture: $m4"
