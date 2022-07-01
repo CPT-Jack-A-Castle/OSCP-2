e844add84d7799c23898f08464e6c0a060de50f6
Stuar-97 <stuartfranklin17@gmail.com>
2022-07-01 10:02:22 -0400

    organized in folders

--- scanning-and-enumeration-tools/nmap-tcp-scanner/nmap-tcp-scanner.sh
+++ scanning-and-enumeration-tools/nmap-tcp-scanner/nmap-tcp-scanner.sh
@@ -5,7 +5,7 @@ then
 	echo "Please enter a valid IP address to scan"
 	echo "Example usage: ./nmap-tcp-scanner.sh <IP>"
 else
-	ports=$(nmap -vv -Pn --min-rate=1000 -p- -T4 $1 | grep [0-9] | cut -d '/' -f1 | tr '\n' ',' | sed s/,$//) 
+	ports=$(nmap -vv -Pn --min-rate=1000 -p- -T4 $1 | grep ^[0-9] | cut -d '/' -f1 | tr '\n' ',' | sed s/,$//) 
 	nmap -Pn $ports -sC -sV $1 -oA $1-tcp-port-scan
 fi
 
